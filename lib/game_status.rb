ed0ec999684db498fbc81be88d69e4bb9a8a6a9c
Kathy Nguyen <kathyvng@gmail.com>
2018-06-02 19:07:40 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,16 +18,23 @@ def check_win_combination?(board, player, win_combo)
   end
 end
 
-def won?(board)
-    WIN_COMBINATIONS.each do |win_combo|
-    if check_win_combination?(board, "X", win_combo)
+def won?
+  WIN_COMBINATIONS.each {|win_combo|
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
+
+    position_1 = @board[index_0]
+    position_2 = @board[index_1]
+    position_3 = @board[index_2]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combo
-    elsif check_win_combination?(board, "O", win_combo)
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combo
-      else
-      return false
-      end
     end
+  }
+  return false
 end
 
 def full?(board)
